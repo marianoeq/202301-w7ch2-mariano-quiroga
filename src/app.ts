bdc2f60f967e4c58e67ebed336185c754770d3b2
mariano quiroga <mariano.eq@gmail.com>
2023-02-28 18:20:57 +0100

    Restructuring of project to config sonar

--- src/app.ts
+++ src/app.ts
@@ -15,18 +15,16 @@ app.use(cors(corsOptions));
 app.use(morgan('dev'));
 app.use(express.json());
 app.use((_req, _res, next) => {
-  console.log();
-  ('soy un middleware');
+  console.log('soy un middleware');
   next();
 });
-//Si tuviera mas rutas, tengo que crear otra linea como la de abajo pero con el nombre de la nueva ruta y luego el nombre del archivo del router.
 app.use('/things', thingsRouter);
 
 app.get('/', (req, res) => {
   res.json({ things: '/things' });
 });
 app.get('/:id', (req, res) => {
-  res.send('' + req.params.id);
+  res.send('id' + req.params.id);
 });
 app.post('/', (req, res) => {
   console.log(req.body);
